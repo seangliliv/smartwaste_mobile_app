4c7bbf00c236e95d8f7ed90a8e4def4735c8b515
Seang Lyliv <seanglyliv@gmail.com>
2025-09-25 12:11:48 +0700

    fetch(loading): add feature loading page

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../utils/app_color.dart';
+import '../widgets/logo_widget.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  void _navigateToLogin() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: LogoWidget(),
+      ),
+    );
+  }
+}
