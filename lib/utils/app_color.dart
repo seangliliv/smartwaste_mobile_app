5aac5e3b697e88466c197d4310938b067dda99e5
Seang Lyliv <seanglyliv@gmail.com>
2025-09-25 12:13:11 +0700

    fetch(utils): add utils

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.green;
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static Color grey600 = Colors.grey[600]!;
+  static Color grey400 = Colors.grey[400]!;
+  static Color grey300 = Colors.grey[300]!;
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,22 @@
+class AppConstants {
+  static const String appName = 'SmartWaste';
+  static const String subtitle = 'Phnom Penh City';
+
+  static const List<String> districts = [
+    'Select Khan (District)',
+    'Khan Chamkar Mon',
+    'Khan Doun Penh',
+    'Khan Prampir Meakkakra',
+    'Khan Tuol Kouk',
+    'Khan Dangkao',
+    'Khan Mean Chey',
+    'Khan Russey Keo',
+    'Khan Sen Sok',
+    'Khan Pur Senchey',
+    'Khan Chrouy Changvar',
+    'Khan Preaek Pnov',
+    'Khan Chbar Ampov',
+    'Khan Boeng Keng Kang',
+    'Khan Kamboul',
+  ];
+}
